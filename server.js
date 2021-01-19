de4054853d29eb6e5928d17c34d04f754676203a
Giang Pham <gpham@veyo.com>
2021-01-19 11:41:33 -0800

    Updated server.js to serve react app

--- server.js
+++ server.js
@@ -46,6 +46,9 @@ var root = {
 //#region STARTING THE EXPRESS SERVER
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Allow cross-origin
 app.use(cors());
 
@@ -58,6 +61,11 @@ app.use(
   })
 );
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
